aef39fba385ede5ea70f9c095d1eaf27dfd43220
Brandur <brandur@brandur.org>
2021-02-16 06:55:28 -0800

    Various minor improvements

--- queries/brandur.org/top_pages_all_time.sql
+++ queries/brandur.org/top_pages_all_time.sql
@@ -1,7 +1,8 @@
-SELECT uri, count(*) as hits
+SELECT uri, count(distinct(request_ip)) as unique_visitors
 FROM brandur_logs.brandur_org
 WHERE status = 200
+  -- filter out static files
   AND uri NOT LIKE '%.%'
 GROUP BY uri
-ORDER BY hits DESC
+ORDER BY unique_visitors DESC
 LIMIT 20;
